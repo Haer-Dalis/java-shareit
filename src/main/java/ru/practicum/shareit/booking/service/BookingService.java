5e976a5c49e938ee1de35bf13ffca18a116a1e0d
HaerDalisBlade <HaerDalisBlade@yandex.ru>
2025-02-23 22:42:21 +0300

    style corrections

--- src/main/java/ru/practicum/shareit/booking/service/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -5,11 +5,17 @@ import ru.practicum.shareit.booking.model.Booking;
 import java.util.List;
 
 public interface BookingService {
+
     Booking addBooking(Integer userId, Integer itemId, Booking booking);
+
     Booking approveBooking(Integer ownerId, Integer bookingId, Boolean approved);
+
     Booking getBooking(Integer userId, Integer bookingId);
+
     List<Booking> getAllUserBookings(Integer userId, String state);
+
     List<Booking> getAllOwnerItemBookings(Integer userId, String state);
+
     Booking getById(Integer id);
 
 }
--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
     boolean existsByEmailAndIdNot(String email, Integer id);
 }
