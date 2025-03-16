46222528865b93a874b6d10445dab92c807ab626
Erensar <richard.fitz.gilbert@yandex.ru>
2025-03-16 20:27:25 +0300

    changes

--- server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestOutputDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/ItemRequestOutputDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.request.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,7 +18,7 @@ public class ItemRequestOutputDto {
 
     private String description;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime created;
 
     private List<ItemDto> items;
