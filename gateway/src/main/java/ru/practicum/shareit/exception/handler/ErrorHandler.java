733390b94b8df113a0690854a30e1bd212dfec90
Erensar <richard.fitz.gilbert@yandex.ru>
2025-03-16 18:31:13 +0300

    logger

--- gateway/src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
@@ -1,5 +1,8 @@
 package ru.practicum.shareit.exception.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,16 +13,26 @@ import ru.practicum.shareit.exception.UnsupportedStatusException;
 @RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
+    @ExceptionHandler(UnsupportedStatusException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnsupportedStatusException(final UnsupportedStatusException exception) {
+        log.error("Ошибка UnsupportedStatusException: {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(UnknownEnumValueException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleEnumValueException(final UnknownEnumValueException exception) {
+        log.error("Ошибка UnknownEnumValueException: {}", exception.getMessage(), exception);
         return new ErrorResponse(exception.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllExceptions(final Exception exception) {
+        log.error("Необработанная ошибка: ", exception);
+        return new ErrorResponse("Внутренняя ошибка сервера: " + exception.getMessage());
+    }
 }
